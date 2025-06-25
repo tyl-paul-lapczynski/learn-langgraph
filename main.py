c977db15e54fa97a750c7d4006e2b9b8af9ff9a2
Paul Lapczynski <paul.lapczynski@tylertech.com>
2025-06-25 11:20:51 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,61 @@
+import os
+from langchain_openai import ChatOpenAI
+from typing import Annotated
+
+from typing_extensions import TypedDict
+
+from langgraph.graph import StateGraph, START
+from langgraph.graph.message import add_messages
+
+
+class State(TypedDict):
+    # Messages have the type "list". The `add_messages` function
+    # in the annotation defines how this state key should be updated
+    # (in this case, it appends messages to the list, rather than overwriting them)
+    messages: Annotated[list, add_messages]
+
+
+graph_builder = StateGraph(State)
+
+
+os.environ["OPENAI_API_KEY"] = "none"
+
+llm = ChatOpenAI(
+    model="google/gemma-3-4b",
+    temperature=0.1,
+    base_url="http://localhost:1234/v1/"
+)
+
+
+def chatBot(state: State):
+    return {"messages": [llm.invoke(state["messages"])]}
+
+
+graph_builder = StateGraph(State)
+
+graph_builder.add_node("chatbot", chatBot)
+
+graph_builder.add_edge(START, "chatbot")
+
+graph = graph_builder.compile()
+
+
+def stream_graph_updates(user_input: str):
+    for event in graph.stream({"messages": [{"role": "user", "content": user_input}]}):
+        for value in event.values():
+            print("Assistant:", value["messages"][-1].content)
+
+
+while True:
+    try:
+        user_input = input("User: ")
+        if user_input.lower() in ["quit", "exit", "q"]:
+            print("Goodbye!")
+            break
+        stream_graph_updates(user_input)
+    except Exception as e:
+        # fallback if input() is not available
+        user_input = "What do you know about LangGraph?"
+        print("User: " + user_input)
+        stream_graph_updates(user_input)
+        break
